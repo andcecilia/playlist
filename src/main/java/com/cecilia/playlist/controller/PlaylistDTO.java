98dbec50b45471f9bc56825c7cc7564d712979d0
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-05-24 11:03:39 -0300

    first commit

---
+++ src/main/java/com/cecilia/playlist/controller/PlaylistDTO.java
@@ -0,0 +1,11 @@
+package com.cecilia.playlist.controller;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class PlaylistDTO {
+        private String singer;
+        private String song;
+    }
---
+++ src/test/java/com/cecilia/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecilia.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
